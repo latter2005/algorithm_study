a0498e6f3c1d4b6c543452a0a05d8432d9176584
latter2005 <latter2005@gmail.com>
2020-09-09 23:20:22 +0900

    프로그래머스 4단고음 문제

---
+++ programmers/2017카카오 예선/4단 고음.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int cnt;
+void calc(int cur, int plus) {
+	if (cur < 1 || 2 * log(cur) / log(3) < plus) return;
+	if (cur == 3) {
+		if (plus ==  2) cnt++;
+		return;
+	}
+	
+	if (cur % 3 == 0 && plus>1) 
+		calc(cur / 3, plus - 2);
+	calc(cur - 1, plus + 1);
+	
+}
+
+int solution(int n) {
+	calc(n - 2, 2);
+	return cnt;
+}
+
+
+
+int main() {
+	//vector<vector<int>> board = { {10, 11, 10, 11}, {2, 21, 20, 10}, {1, 20, 21, 11}, {2, 1, 2, 1} };
+
+
+	cout << solution(24);
+}
+
+/*
+하양식 풀이로 탐색횟수 줄임, 남은 별 개수와 현재 + 개수를 비교해 +개수가 2*남은 별 개수보다 크다면 종료조건
+*/
