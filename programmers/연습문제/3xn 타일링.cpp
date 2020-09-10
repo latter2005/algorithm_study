ca5a98db45aae69cf8fd77ddc47eabd4d4f629d6
latter2005 <latter2005@gmail.com>
2020-09-10 23:10:24 +0900

    프로그래머스 3 x n 타일링 문제

---
+++ programmers/연습문제/3xn 타일링.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int solution(int n) {
+	if (n % 2) return 0;
+	else {
+		unsigned long long f_2 = 3, prev = 3;
+		unsigned long long sum = 3, cur = 3;
+		for (int i = 2; i < n; i += 2) {
+			cur = prev + sum * 2 + 2 ;
+			cur %= 1000000007;
+			prev = cur;
+			sum += cur;
+		}
+		return (int)(cur % 1000000007);
+	}
+
+}
+
+
+int main() {
+	cout << solution(40);
+}
+
+/*
+점화식 f(n) = 3f(n-2) + 2f(n-4) + ~ + 2f(2) + 2
+총 합 변수와 그전 단계의 값만 저장을 하여 메모리 사용 줄임
+*/
