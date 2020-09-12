8d98fa8e7d2ecbb85588cec22985c59c159b0e42
latter2005 <latter2005@gmail.com>
2020-09-13 07:32:27 +0900

    다익스트라, 세그먼트 트리 수정

--- 유용한 코드들/segment tree/recursive and lazy p.cpp
+++ 유용한 코드들/segment tree/recursive and lazy p.cpp
@@ -1,44 +1,84 @@
-﻿//segment tree 리커시브 with lazy propagation
+﻿//segment tree iterative 버전
 #include <cstdio>
 #include <vector>
 #include <queue>
 #include <cmath>
 using namespace std;
-#define MAZ_SIZE 10000
-typedef struct node {
-	int val, lazy;
-}node;
+#define MAX_SIZE 100000
 
 /*
 H : log(N)(올림)
 8 -> 3, 2^4-1 = 15
 9 -> 4, 2^5-1 = 31
-1000 -> 10, 2^11-1 = 2047 
-10000 -> 14, 2^15-1
 node 개수 : 2^(H+1)-1
+int h = (int)ceil(log2(n));
+int tree_size = (1 << (h+1)) - 1;
 */
-node tree[(2<<15)-1];
-vector<int> *arr_ptr;
- int init(int index, int start, int end) {
-	if (start == end)
-		return tree[index].val = (*arr_ptr)[start];
+
+
+
+int init(vector<int> &a, vector<int> &tree, int node, int start, int end) {
+	if (start == end) {
+		return tree[node] = a[start];
+	}
 	else {
-		int mid = (start + end) / 2;
-		return tree[index].val = init((index << 1) + 1, start, mid) +
-			init((index << 1) + 2, mid + 1, end);
+		return tree[node] = init(a, tree, node * 2, start, (start + end) / 2) + init(a, tree, node * 2 + 1, (start + end) / 2 + 1, end);
 	}
 }
-
-
-int solution(vector<int> arr) {
-	int answer=1;
-	int arr_size = arr.size();
-	init(0, 0, arr_size - 1);
-
-	return answer;
+void update_lazy(vector<int> &tree, vector<int> &lazy, int node, int start, int end) {
+	if (lazy[node] != 0) {
+		tree[node] += (end - start + 1)*lazy[node];
+		// leaf가 아니면
+		if (start != end) {
+			lazy[node * 2] += lazy[node];
+			lazy[node * 2 + 1] += lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+void update_range(vector<int> &tree, vector<int> &lazy, int node, int start, int end, int left, int right, int diff) {
+	update_lazy(tree, lazy, node, start, end);
+	if (left > end || right < start) {
+		return;
+	}
+	if (left <= start && end <= right) {
+		tree[node] += (end - start + 1)*diff;
+		if (start != end) {
+			lazy[node * 2] += diff;
+			lazy[node * 2 + 1] += diff;
+		}
+		return;
+	}
+	update_range(tree, lazy, node * 2, start, (start + end) / 2, left, right, diff);
+	update_range(tree, lazy, node * 2 + 1, (start + end) / 2 + 1, end, left, right, diff);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+int sum(vector<int> &tree, vector<int> &lazy, int node, int start, int end, int left, int right) {
+	update_lazy(tree, lazy, node, start, end);
+	if (left > end || right < start) {
+		return 0;
+	}
+	if (left <= start && end <= right) {
+		return tree[node];
+	}
+	return sum(tree, lazy, node * 2, start, (start + end) / 2, left, right) + sum(tree, lazy, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
 }
 
+int solution(vector<int> arr) {
+	int answer = 1;
+	int arr_size = arr.size();
+	int h = (int)ceil(log2(arr_size));
+	int tree_size = (1 << (h + 1)) - 1;
+	vector<int> tree(tree_size);
+	vector<int> lazy(tree_size);
+	init(arr, tree, 1, 0, arr_size-1);
+	update_range(tree, lazy, 1, 0, arr_size - 1, 0, 5, 1);
+	return sum(tree, lazy, 1, 0, arr_size - 1, 0, 4);
+}
+
+
 int main() {
+	
 	vector<int> arr = { 1, 7, 5, 6, 10 };
-	solution(arr);
+	printf("%d", solution(arr));
 }
---
+++ 유용한 코드들/다익스트라/다익스트라.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+#define INF 0x7fffffff
+#define MAX_SIZE 100
+using namespace std;
+
+int solution(vector<vector<int>> arr,int n, int start) {
+	vector<pair<int, int>> graph[MAX_SIZE];
+	int dist[MAX_SIZE];
+	fill(dist, dist + MAX_SIZE, INF);
+	for (auto &i : arr) {
+		graph[i[0]].push_back({ i[1],i[2] });
+		graph[i[1]].push_back({ i[0],i[2] });
+	}
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+	que.push({ start, 0 });
+	dist[start] = 0;
+	while (!que.empty()) {
+		auto &t = que.top();
+		int cur = t.first, cur_cost = t.second;
+		que.pop();
+
+		auto &cur_vtx = graph[cur];
+		for (int i = 0; i < cur_vtx.size(); i++) {
+			int next = cur_vtx[i].first, next_cost = cur_vtx[i].second;
+			if (dist[next] > dist[cur] + next_cost) {
+				dist[next] = dist[cur] + next_cost;
+				que.push({ next, dist[next] });
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", dist[i]);
+	}
+	return 0;
+}
+
+int main(){
+	
+	vector<vector<int>> arr = { {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}} };
+	solution(arr, 6, 4);
+	
+}
