39f52fe45b34243cf5b0c4d21ad99225ccd23797
latter2005 <latter2005@gmail.com>
2020-11-02 14:28:26 +0900

    백준 나무 재테크 문제

---
+++ 백준/문제집/삼성 SW/16235/나무 재테크.cpp
@@ -0,0 +1,89 @@
+﻿#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int dx[] = { -1, -1, -1, 0, 0, 1, 1, 1 },
+dy[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+vector<pair<int, int>> tree[10][10];//level cnt
+int ary[10][10], s2d2[10][10];
+int N, M, K;
+void spring_summer_winter() {
+	//spring
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++) {
+
+			int total_add = s2d2[i][j];
+			for (int t = tree[i][j].size() - 1; t >= 0; t--) {
+				int level = tree[i][j][t].first;
+
+				if (level * tree[i][j][t].second <= ary[i][j]) {
+					ary[i][j] -= level * tree[i][j][t].second;
+					tree[i][j][t].first++;
+				}
+				else {
+					//summer
+					int e = t + 1;
+					int alive = ary[i][j] / level, dead = tree[i][j][t].second - alive;
+					if (alive) {
+						ary[i][j] -= alive * level;
+						tree[i][j][t].first++;
+						tree[i][j][t].second = alive;
+						e--;
+					}
+					total_add += dead * (level >> 1);
+					for (t--; t >= 0; t--)
+						total_add += (tree[i][j][t].first >> 1) * tree[i][j][t].second;
+					tree[i][j].erase(tree[i][j].begin(), tree[i][j].begin() + e);
+
+				}
+			}
+			ary[i][j] += total_add; //winter
+		}
+}
+void winter() {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			for (auto &t : tree[i][j])
+				if (t.first % 5 == 0)
+					for (int d = 0; d < 8; d++) {
+						int nx = dx[d] + i, ny = dy[d] + j;
+						if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+							if (tree[nx][ny].size() && tree[nx][ny].back().first == 1)
+								tree[nx][ny].back().second += t.second;
+							else
+								tree[nx][ny].emplace_back(1, t.second);
+						}
+
+					}
+}
+void solve() {
+	int cnt = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			for (auto t : tree[i][j])
+				cnt += t.second;
+	printf("%d", cnt);
+}
+int main() {
+	
+	scanf("%d%d%d", &N, &M, &K);
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++) {
+			ary[i][j] = 5;
+			scanf("%d", &s2d2[i][j]);
+		}
+	int x, y, z;
+	for (int i = 0; i < M; i++) {
+		scanf("%d%d%d", &x, &y, &z);
+		tree[--x][--y].emplace_back( z, 1 );
+	}
+
+	while (K--) {
+		spring_summer_winter();
+		winter();
+	}
+	solve();
+}
+
--- 유용한 코드들/fast_io.cpp
+++ 유용한 코드들/fast_io.cpp
@@ -1,3 +1,4 @@
+//윈도우
 class fast_io {
 public:
 	int next_int() {
@@ -17,3 +18,22 @@ public:
 	}
 };
 fast_io fio;
+
+
+//리눅스
+
+#include <unistd.h>
+namespace io {
+	const signed IS=1<<22, OS=1<<20;
+	char I[IS+1],*J=I,O[OS],*K=O;
+
+	inline void daer(){if(J>=I+IS-64){char*p=I;do*p++=*J++;while(J!=I+IS);p[read(0,p,I+IS-p)]=0;J=I;}}
+	template<int N=10,typename T=int>inline T getu(){daer();T x=0;int k=0;do x=x*10+*J-'0';while(*++J>='0'&&++k<N);++J;return x;}
+	template<int N=10,typename T=int>inline T geti(){daer();bool e=*J=='-';J+=e;return(e?-1:1)*getu<N,T>();}
+	inline void flush(){write(1,O,K-O);K=O;}
+
+	template<int N=10,typename T=int>inline void putu(T n){char s[(N+7)/8*8],*p=s;int k=0;do*p++=n%10+48;while((n/=10)&&++k<N);do*K++=*--p;while(p!=s);*K++=10;if(K>=O+OS-64)flush();}
+	template<int N=10,typename T=int>inline void puti(T n){if(n<0)*K++='-',n=-n;putu<N,T>(n);}
+	struct f{f(){I[read(0,I,IS)]=0;}~f(){flush();}}flu;
+};
+using namespace io;
