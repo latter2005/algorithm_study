ce696368a9111a259b0cf46d436654513a732f72
latter2005 <latter2005@gmail.com>
2020-11-02 18:00:58 +0900

    나무 재테크 문제 주석 추가

--- 백준/문제집/삼성 SW/16235/나무 재테크(c).cpp
+++ 백준/문제집/삼성 SW/16235/나무 재테크(c).cpp
@@ -16,61 +16,57 @@ void spring_summer_winter() {
 				char new_max = 0;
 				for (char t = 1; t <= tree[i][j].max_level; t++) {
 					if (tree[i][j].level[t]) {
-						if (t * tree[i][j].level[t] <= tree[i][j].resource) {
+						if (t * tree[i][j].level[t] <= tree[i][j].resource) {//나이대 모든 나무를 살릴 수 있는 경우
 							tree[i][j].resource -= t * tree[i][j].level[t];
-							new_max = t;
+							new_max = t;//최대 나이대를 계속해서 갱신 해야함
 						}
-						else {
+						else {//일부 혹은 모든 나무가 죽는 경우
 							short alive = tree[i][j].resource / t, dead = tree[i][j].level[t] - alive;
 							if (alive) {
 								tree[i][j].level[t] = alive;
 								tree[i][j].resource -= alive * t;
-								new_max = t;
+								new_max = t;//나무가 일부 살아있으면 최대나이로 설정, 모두 죽는 경우면 그전 단계(or 0)으로 설정
 							}
 							//summer
 							total_add += dead * (t >> 1);
 							for (t++; t <= tree[i][j].max_level; t++)
 								if (tree[i][j].level[t])
-									total_add += (t >> 1) *  tree[i][j].level[t];
+									total_add += (t >> 1) *  tree[i][j].level[t];//죽은 나무들 양분으로 변경
 							break;
 						}
 					}
 				}
-				tree[i][j].max_level = new_max;
+				tree[i][j].max_level = new_max;//최대 나무 나이 갱신
 				if (new_max)
 					for (short t = tree[i][j].max_level++; t >= 1; t--)
-						tree[i][j].level[t + 1] = tree[i][j].level[t];
+						tree[i][j].level[t + 1] = tree[i][j].level[t]; //살아 있는 나무들 나이 증가
 				tree[i][j].level[1] = 0;
-
 			}
 			tree[i][j].resource += total_add; //winter
 		}
+	//autumn
 	for (char i = 0; i < N; i++)
 		for (char j = 0; j < N; j++) {
 			short total_count = 0;
 			for (short t = 5; t <= tree[i][j].max_level; t += 5) {
 				if (tree[i][j].level[t])
-					total_count += tree[i][j].level[t];
+					total_count += tree[i][j].level[t];//번식할 나무 개수 구하기
 			}
 			if (total_count) {
 				for (char d = 0; d < 8; d++) {
 					short nx = dx[d] + i, ny = dy[d] + j;
 					if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-						tree[nx][ny].level[1] += total_count;
+						tree[nx][ny].level[1] += total_count;//개수 만금 8방향으로 전파
 						if (!tree[nx][ny].max_level)
-							tree[nx][ny].max_level = 1;
+							tree[nx][ny].max_level = 1;//나무가 없는 조건을 max_count로 하였기 때문에 max_count 변경 해 주어야 잘 돌아감
 					}
 				}
 			}
 		}
 }
-
-
-
 int main() {
 	short M, K;
 	scanf("%hd%hd%hd", &N, &M, &K);
-
 	for (char i = 0; i < N; i++)
 		for (char j = 0; j < N; j++) {
 			scanf("%hd", &tree[i][j].s2d2);
@@ -82,7 +78,6 @@ int main() {
 		tree[--x][--y].level[z] = 1;
 		tree[x][y].max_level = z;
 	}
-
 	while (K--) {
 		spring_summer_winter();
 	}
@@ -94,4 +89,3 @@ int main() {
 					cnt += tree[i][j].level[t];
 	printf("%hd", cnt);
 }
-
--- 백준/문제집/삼성 SW/16235/나무 재테크.cpp
+++ 백준/문제집/삼성 SW/16235/나무 재테크.cpp
@@ -1,41 +1,36 @@
 ﻿#include <cstdio>
 #include <vector>
-
 using namespace std;
-
 int dx[] = { -1, -1, -1, 0, 0, 1, 1, 1 },
 dy[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-vector<pair<int, int>> tree[10][10];//level cnt
+vector<pair<int, int>> tree[10][10];//나이 개수
 int ary[10][10], s2d2[10][10];
 int N, M, K;
 void spring_summer_winter() {
 	//spring
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++) {
-
 			int total_add = s2d2[i][j];
 			for (int t = tree[i][j].size() - 1; t >= 0; t--) {
 				int level = tree[i][j][t].first;
-
-				if (level * tree[i][j][t].second <= ary[i][j]) {
+				if (level * tree[i][j][t].second <= ary[i][j]) {//그 나이대 나무를 모두 살릴 수 있는 경우
 					ary[i][j] -= level * tree[i][j][t].second;
 					tree[i][j][t].first++;
 				}
-				else {
-					//summer
+				else {//일부 혹은 전부 죽는 경우
 					int e = t + 1;
 					int alive = ary[i][j] / level, dead = tree[i][j][t].second - alive;
 					if (alive) {
 						ary[i][j] -= alive * level;
 						tree[i][j][t].first++;
 						tree[i][j][t].second = alive;
-						e--;
+						e--;//아직 살아있는 나무가 있기 때문에 삭제할 벡터 위치를 당김
 					}
-					total_add += dead * (level >> 1);
-					for (t--; t >= 0; t--)
+					//summer
+					total_add += dead * (level >> 1); //죽은 나무 양분으로 변경
+					for (t--; t >= 0; t--)//뒤에 나이가 더 많은 나무들을 모두 죽임
 						total_add += (tree[i][j][t].first >> 1) * tree[i][j][t].second;
-					tree[i][j].erase(tree[i][j].begin(), tree[i][j].begin() + e);
-
+					tree[i][j].erase(tree[i][j].begin(), tree[i][j].begin() + e);//지정된 위치까지 나무들 정보 삭제
 				}
 			}
 			ary[i][j] += total_add; //winter
@@ -54,7 +49,6 @@ void winter() {
 							else
 								tree[nx][ny].emplace_back(1, t.second);
 						}
-
 					}
 }
 void solve() {
@@ -68,7 +62,6 @@ void solve() {
 int main() {
 	
 	scanf("%d%d%d", &N, &M, &K);
-
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++) {
 			ary[i][j] = 5;
@@ -79,11 +72,9 @@ int main() {
 		scanf("%d%d%d", &x, &y, &z);
 		tree[--x][--y].emplace_back( z, 1 );
 	}
-
 	while (K--) {
 		spring_summer_winter();
 		winter();
 	}
 	solve();
 }
-
