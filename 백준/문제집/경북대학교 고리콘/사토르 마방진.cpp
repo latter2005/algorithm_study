7612b2e6052e4b71886aa59a756d4b0fc41f080f
latter2005 <latter2005@gmail.com>
2020-11-10 00:38:39 +0900

    경북대 고리콘 문제들 1. 사토르 마방진

---
+++ 백준/문제집/경북대학교 고리콘/사토르 마방진.cpp
@@ -0,0 +1,27 @@
+﻿#include <cstdio>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, max_val = 0x7fffffff + 1, res = 0x7fffffff + 1;
+int ary[1000];
+
+
+
+int main() {
+	int combo = 1;
+	int dp[1000][1000][3];//no 1, 2
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &ary[i]);
+
+	for (int i = 0; i < n; i++) {
+		
+			combo++;
+	}
+	printf("%d", cur);
+}
+/*
+4
+3 4 -7 8
+*/
