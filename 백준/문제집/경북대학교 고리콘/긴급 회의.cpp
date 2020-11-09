d7ca869391ff37945a69166663a0e10e2346bf78
latter2005 <latter2005@gmail.com>
2020-11-10 02:55:21 +0900

    고리콘 긴급 회의 문제

---
+++ 백준/문제집/경북대학교 고리콘/긴급 회의.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main() {
+	int n, t;
+	char ary[101] = "", dup = 0, max = 1;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &t);
+		ary[t]++;
+	}
+	for (int i = 2; i <= n; i++) {
+		if (ary[max] < ary[i]) {
+			max = i;
+			dup = 0;
+		}
+		else if (ary[max] == ary[i])
+			dup = 1;
+	}
+	printf((dup || !ary[max]) ? "skipped" : "%d", max);	
+}
