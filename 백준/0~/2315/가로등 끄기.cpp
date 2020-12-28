be0ab33fc31e550fecfdae53e6042fa544359f82
latter2005 <latter2005@gmail.com>
2020-12-29 06:06:55 +0900

    가로등 문제 반복문 사용

--- 백준/0~/2315/가로등 끄기.cpp
+++ 백준/0~/2315/가로등 끄기.cpp
@@ -1,39 +1,31 @@
 ﻿#include <cstdio>
 #include <algorithm>
-
 using namespace std;
-int n, m, ary[1001], w[1001] = { 0 }, wt, dp[1001][1001][2];
-int solve(int left, int right, bool tag) {
-	if (dp[left][right][tag])return dp[left][right][tag];
-	else if (left==1 && right==n) return dp[left][right][tag] = 0;
-	int val = 0x7fffffff, consume = wt - (w[right] - w[left - 1]);
 
-	if (left>1)
-		val = solve(left - 1, right, false) +
-		consume * (tag ? ary[right] - ary[left - 1] : ary[left] - ary[left - 1]);
-	if (right<n)
-		val = min(val, solve(left, right + 1, true) +
-			consume * (tag ? ary[right + 1] - ary[right] : ary[right + 1] - ary[left]));
-	return dp[left][right][tag] = val;
-}
 int main() {
-	
+	int n, m, ary[1001], w[1001] = { 0 }, wt, dp[1001][1001][2] = { 0 };
 	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= n; i++) {
 		scanf("%d%d", &ary[i], &w[i]);
 		w[i] += w[i - 1];
 	}
 	wt = w[n];
-
-	printf("%d", solve(m,m, false));
+	int right = n - 1;
+	for (int left = 1; left <= m; ++left) {
+		for (; right >= m; --right) {
+			int x = 0x7fffffff, y = 0x7fffffff, consume = wt - (w[right] - w[left - 1]);
+			if (left != 1) {
+				x = min(x, dp[left - 1][right][0] + consume * (ary[left] - ary[left - 1]));
+				y = min(y, dp[left - 1][right][0] + consume * (ary[right] - ary[left - 1]));
+			}
+			if (right != n) {
+				x = min(x, dp[left][right + 1][1] + consume * (ary[right + 1] - ary[left]));
+				y = min(y, dp[left][right + 1][1] + consume * (ary[right + 1] - ary[right]));
+			}
+			dp[left][right][0] = x;
+			dp[left][right][1] = y;
+		}
+		right = n;
+	}
+	printf("%d", min(dp[m][m][0], dp[m][m][1]));
 }
-
-/*
-6 5
-3 2
-11 10
-12 18
-13 19
-15 15
-17 19
-*/
