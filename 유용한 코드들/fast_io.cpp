0fab356a7275ff67809c2ec8e9c10214f5964fc6
latter2005 <latter2005@gmail.com>
2020-12-30 03:02:20 +0900

    일반버전 수정

--- 유용한 코드들/fast_io.cpp
+++ 유용한 코드들/fast_io.cpp
@@ -7,21 +7,23 @@ ios::sync_with_stdio(false);
 //윈도우
 class fast_io {
 public:
-	int next_int() {
-		int x = 0;
-		bool neg = false;
-		register int c;
-		c = _getchar_nolock();
-		if (c == '-') {
-			neg = true;
-			c = getchar();
-		}
-		for (; (c > 47 && c < 58); c = _getchar_nolock())
-			x = (x << 1) + (x << 3) + c - 48;
-		if (neg)
-			x *= -1;
-		return x;
+int next_int() {
+	int x = 0;
+	bool neg = false;
+	register int c;
+	c = getchar_unlocked();
+	while (c != '-' && (c < 48 || c > 57))
+		c = getchar_unlocked();
+	if (c == '-') {
+		neg = true;
+		c = getchar();
 	}
+	for (; (c > 47 && c < 58); c = getchar_unlocked())
+		x = (x << 1) + (x << 3) + c - 48;
+	if (neg)
+		x *= -1;
+	return x;
+}
 };
 fast_io fio;
 
