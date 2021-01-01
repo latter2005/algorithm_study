aa787f79cad05262d369e6670ec321fa2f3005b1
latter2005 <latter2005@gmail.com>
2021-01-01 18:25:51 +0900

    백준 구간 합 구하기 문제

---
+++ 백준/0~/2042/구간 합 구하기.cpp
@@ -0,0 +1,33 @@
+﻿//https://latter2005.tistory.com/40
+#include <cstdio>
+using namespace std;
+
+int main() {
+	long long n, m, k;
+	long long tree[2000002];
+	scanf("%lld%lld%lld", &n, &m, &k);
+	for (int i = 0; i < n; i++)
+		scanf("%lld", &tree[n + i]);
+	for (int i = n - 1; i > 0; i--)
+		tree[i] = tree[i << 1] + tree[i << 1 | 1];
+	for (int i = 0; i < m + k; i++) {
+		long long a, b, c;
+		scanf("%lld%lld%lld", &a, &b, &c);
+		if (a == 1) {
+			b += n - 1;
+			long long gap = c - tree[b];
+			tree[b] = c;
+			for (b >>= 1; b >= 1; b >>= 1) {
+				tree[b] += gap;
+			}
+		}
+		else {
+			long long sum = 0;
+			for (b += n - 1, c += n; b < c; b >>= 1, c >>= 1) {
+				if (b & 1) sum += tree[b++];
+				if (c & 1) sum += tree[--c];
+			}
+			printf("%lld\n", sum);
+		}
+	}
+}
