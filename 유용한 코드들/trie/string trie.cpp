0ea4b10ae791687ec4ecd0337132f7be777ec74e
latter2005 <latter2005@gmail.com>
2021-01-15 18:00:25 +0900

    trie find & insert 함수 추가

--- 유용한 코드들/trie/string trie.cpp
+++ 유용한 코드들/trie/string trie.cpp
@@ -1,3 +1,7 @@
+#include <string>
+#include <vector>
+#include <cstdio>
+using namespace std;
 struct Trie {
 	bool finish;    //끝나는 지점을 표시해줌
 	int count = 0;
@@ -15,7 +19,7 @@ struct Trie {
 		Trie* cur_node = this;
 		for (int i = 0; i < n; i++) {//for(auto &t : key)
 			cur_node->count++;
-			
+
 			int cur_idx = key[i] - 'a';
 			if (cur_node->next[cur_idx] == NULL)
 				cur_node->next[cur_idx] = new Trie();
@@ -36,6 +40,24 @@ struct Trie {
 		if (cur_node->finish) return cur_node;
 		else return NULL;
 	}
+	bool find_and_insert(string key) {
+		int n = key.size();
+		Trie* cur_node = this;
+		for (int i = 0; i < n; i++) {//for(auto &t : key)
+			cur_node->count++;
+
+			int cur_idx = key[i] - 'a';
+			if (cur_node->next[cur_idx] == NULL)
+				cur_node->next[cur_idx] = new Trie();
+
+			cur_node = cur_node->next[cur_idx];
+		}
+		if (cur_node->finish)
+			return true;
+		cur_node->count++;
+		cur_node->finish = true;
+		return false;
+	}
 	int counting(string key) {
 		int n = key.size();
 		Trie* cur_node = this;
@@ -48,3 +70,13 @@ struct Trie {
 		return cur_node->count;
 	}
 };
+
+int main() {
+	vector<string> t = { "asdf", "asdf", "sss" };
+	Trie trie;
+	bool c;
+	for (auto s : t) {
+		c = trie.find_and_insert(s);
+	}
+	
+}
