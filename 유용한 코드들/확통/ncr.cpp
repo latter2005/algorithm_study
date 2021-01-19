93ee7c353ca696be40210a6dfd83ef34641d04e8
latter2005 <latter2005@gmail.com>
2021-01-19 22:59:26 +0900

    조합 함수 ncr 추가

---
+++ 유용한 코드들/확통/ncr.cpp
@@ -0,0 +1,12 @@
+long long C(int n, int r) {
+    if(r > n - r) r = n - r; // because C(n, r) == C(n, n - r)
+    long long ans = 1;
+    int i;
+
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+
+    return ans;
+}
