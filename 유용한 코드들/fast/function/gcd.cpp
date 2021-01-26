cee5af2929e3a32a75200363f2ad7da981bc8ee5
latter2005 <latter2005@gmail.com>
2021-01-26 20:01:26 +0900

    lcm 추가

--- 유용한 코드들/fast/function/gcd.cpp
+++ 유용한 코드들/fast/function/gcd.cpp
@@ -14,6 +14,15 @@ int gcd2(int a, int b)
 	while (b) b ^= a ^= b ^= a %= b;
 	return a;
 }
+long lcm(long a, long b) {
+	long r, ta = a, tb = b;
+	while (tb != 0) {
+		r = ta % tb;
+		ta = tb;
+		tb = r;
+	}
+	return a * b / ta;
+}
 int main() {
 	printf("%d", gcd2(35, 49));
 }
