14c5817c15ac6d3d8c586473bd12fb8782ae482b
latter2005 <latter2005@gmail.com>
2021-02-22 21:52:56 +0900

    펜윅 트리 코드

---
+++ 유용한 코드들/자료구조/펜윅 트리/fenwick.cpp
@@ -0,0 +1,50 @@
+//https://www.crocus.co.kr/666
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+typedef long long ll;
+using namespace std;
+int n, m;
+int ary[500001];
+
+inline void update(int i, int val) {
+	for (; i <= n; i += i & -i)
+		ary[i] ^= val;
+}
+inline int query(int i) {
+	int t = 0;
+	for (; i; i -= i & -i)
+		t ^= ary[i];
+	return t;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	for (int i = 0, t; i < n; i++) {
+		cin >> t;
+		update(i + 1, t);
+		update(i + 2, t);
+	}
+
+
+	cin >> m;
+	while (m--) {
+		int op;
+		cin >> op;
+		if (op != 2) {
+			int s, e, v;
+			cin >> s >> e >> v;
+			update(s + 1, v);
+			update(e + 2, v);
+		}
+		else {
+			int i;
+			cin >> i;
+			cout << query(i + 1) << '\n';
+		}
+	}
+}
