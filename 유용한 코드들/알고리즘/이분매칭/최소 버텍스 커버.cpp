9586085e787c4249de51d41a4fdf513f551289ac
latter2005 <latter2005@gmail.com>
2021-02-22 22:35:32 +0900

    구조 변경, 이분 매칭 최소버텍스 커버 코드

---
+++ 유용한 코드들/알고리즘/이분매칭/최소 버텍스 커버.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <vector>
+#include <memory.h>
+using namespace std;
+vector <short> graph[501];
+short match[501];
+bool visit[501];
+bool dfs(short i) {
+    for (short next : graph[i]) {
+        if (visit[next])continue;
+        visit[next] = true;
+        if (!match[next] || dfs(match[next])) {
+            match[next] = i;
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    int n, k, res = 0;
+ 
+    scanf("%d%d", &n, &k);
+    for (short i = 0, s, e; i < k; i++) {
+        scanf("%hd%hd", &s, &e);
+        graph[s].push_back(e);
+    }
+    for (short i = 1; i <= n; i++) {
+        memset(visit, 0, n+1);
+        if (dfs(i))res++;
+    }
+    printf("%hd", res);
+}
