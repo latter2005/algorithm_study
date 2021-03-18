e8c529b99f134572278a0bb31508cece55a82018
latter2005 <latter2005@gmail.com>
2021-03-18 22:53:01 +0900

    오류 수정

--- 유용한 코드들/fast/function/string split/substr.cpp
+++ 유용한 코드들/fast/function/string split/substr.cpp
@@ -3,8 +3,8 @@ vector<string> split(string &input, char delimiter) {
 	
 	for (int i = 0, prev = 0; i < input.size();) {
 		while (i < input.size() && input[i] != delimiter)++i;
-		answer.push_back(input.substr(prev, i + 1));
-		prev = i + 1;
+		t.push_back(s.substr(prev, i - prev));
+		prev = ++i;
 	}
 
 	return answer;
