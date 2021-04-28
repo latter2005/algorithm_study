ea5bc6762d0b0b27e8d1d4391c26103f12452587
latter2005 <latter2005@gmail.com>
2021-04-28 22:51:43 +0900

    1, 2번 문제

---
+++ programmers/월간 코드 챌린지 시즌2/괄호 회전하기.cpp
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int solution(string s) {
+	int answer = 0;
+	int len = s.length();
+
+	for (int i = 0; i < len; i++) {
+		stack<char> stk;
+
+		bool f = true;
+
+		for (int j = 0; f && j < len; j++) {
+			if (s[j] == '(' || s[j] == '[' || s[j] == '{')
+				stk.push(s[j]);
+			else if (s[j] == ')') {
+				if (stk.empty() || stk.top() != '(') {
+					f = false;
+					break;
+				}
+				stk.pop();
+			}
+			else if (s[j] == ']') {
+				if (stk.empty() || stk.top() != '[') {
+					f = false;
+					break;
+				}
+				stk.pop();
+			}
+			else if (s[j] == '}') {
+				if (stk.empty() || stk.top() != '{') {
+					f = false;
+					break;
+				}
+				stk.pop();
+			}
+		}
+		if (stk.empty() && f)
+			answer++;
+		s.insert(s.begin(), s.back());
+		s.pop_back();
+	}
+
+	return answer;
+}
+int main() {
+	string t = "[]() {}";
+	solution(t);
+}
---
+++ programmers/월간 코드 챌린지 시즌2/음양 더하기.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+	int answer = 0;
+	
+	int n = absolutes.size();
+
+	for (int i = 0; i < n; i++)
+		answer += signs[i] ? absolutes[i] : -absolutes[i];
+	return answer;
+}
