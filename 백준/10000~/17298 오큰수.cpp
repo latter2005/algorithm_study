65d040389190c836c9600766ad6335a7771f4634
latter2005 <latter2005@gmail.com>
2021-05-05 19:37:52 +0900

    오큰수 인덱싱 버전

--- 백준/10000~/17298 오큰수.cpp
+++ 백준/10000~/17298 오큰수.cpp
@@ -2,18 +2,19 @@
 
 int main() {
 	int n;
-	int ary[1000000], stk[1000000], ans[1000000], top = 1;
+	int ary[1000000], nge[1000000];
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &ary[i]);
-	ans[n - 1] = -1;
-	stk[0] = ary[n - 1];
+	nge[n - 1] = -1;
+
 
 	for (int i = n - 2; i >= 0; i--) {
-		while (top && ary[i] >= stk[top - 1])--top;
-		ans[i] = top ? stk[top - 1] : -1;
-		stk[top++] = ary[i];
+		int idx = i + 1;
+		while (idx != -1 && ary[idx] <= ary[i])
+			idx = nge[idx];
+		nge[i] = idx;
 	}
 	for (int i = 0; i < n; i++)
-		printf("%d ", ans[i]);
+		printf("%d ", nge[i] != -1 ? ary[nge[i]] : -1);
 }
