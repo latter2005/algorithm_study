0cf36b122ae86b8b1d37731b244254880666c0f3
latter2005 <latter2005@gmail.com>
2021-05-14 18:46:09 +0900

    프로그래머스 약수의 개수와 덧셈 문제

---
+++ programmers/월간 코드 챌린지 시즌2/약수의 개수와 덧셈.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+	int answer = 0;
+
+	int visited[1001] = { 0 };
+
+	for (int i = 1; i*i <= right; i++)
+		visited[i*i] = 1;
+	
+	for (int i = left; i <= right; i++)
+		answer += visited[i] ? -i : i;
+	return answer;
+}
+
+int main() {
+	solution(13, 17);
+}
